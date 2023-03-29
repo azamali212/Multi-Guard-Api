76111ba2056f888c37836cf80ddb329a5bd51193
azamali212 <azamalifsd73@gmail.com>
2023-03-30 03:53:10 +0500

    Update New additionx

--- routes/api.php
+++ routes/api.php
@@ -12,20 +12,6 @@ use App\Http\Controllers\Teacher\TeacherLoginController;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Models\Role;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 // Admin Route
 Route::post('admin/login', [App\Http\Controllers\Admin\LoginController::class, 'AdminLogin']);
